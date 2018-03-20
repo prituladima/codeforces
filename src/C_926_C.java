4748b076f97a35e8c920d6ab4189e4fe2a7a3bd5
prituladima <prituladima@gmail.com>
2018-03-21 00:40:32 +0200

    C_926_C [failed]

--- src/C_926_C.java
+++ src/C_926_C.java
@@ -1,4 +1,6 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * Created by prituladima on 3/20/18.
@@ -10,12 +12,21 @@ public class C_926_C {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] array = new int[n];
-        for (int i = 0; i < n; i++) {
-            array[i] = scanner.nextInt();
-        }
-        //todo
 
-        System.out.println("YES");
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(array[i] = scanner.nextInt());
+        }
+
+        if(set.size() == 1){
+            System.out.println("YES");
+            return;
+        }
+
+
+
+
+
 
     }
 
