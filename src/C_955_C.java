706f0096e9b991fdb8d33bfde46cd103ae7f729e
prituladima <prituladima@gmail.com>
2018-04-02 22:29:37 +0300

    ---

--- src/C_955_C.java
+++ src/C_955_C.java
@@ -1,106 +1,193 @@
-import org.omg.CORBA.INTERNAL;
+//package round474;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
 
-/**
- * Created by prituladima on 3/23/18.
- */
-public class C_955_C {
+public class C {
+    InputStream is;
+    PrintWriter out;
+    String INPUT = "";
 
-
-    public static void main(String[] args) throws IOException {
-
-        FastScanner scanner = new FastScanner(System.in);
-        int Q = scanner.nextInt();
-
-        List<Integer> Ls = new ArrayList<>();
-        List<Integer> Rs = new ArrayList<>();
-        for (int i = 0; i < Q; i++) {
-            Ls.add(scanner.nextInt());
-            Rs.add(scanner.nextInt());
-        }
-
-        for (int i = 0; i < Ls.size(); i++) {
-            System.out.println(solve(Ls.get(i), Rs.get(i)));
-        }
-
-
-    }
-
-    public static long solve(long L, long R) {
-        long ar = 1000;
-        long pr = 70;
-
-        Set<Long> set = new HashSet<>();
-
-        for (long a = 1; a < ar; a++) {
-            for (long p = 2; p < pr; p++) {
-                long current = fast_pow(a, p);
-                if (L <= current && current <= R) {
-                    set.add(current);
+    void solve() {
+        long[] all = new long[1500000];
+        int p = 0;
+        for (long i = 2; i <= 1000000; i++) {
+            long x = 1;
+            for (int e = 1; ; e++) {
+                double y = (double) x * i;
+                if (y > 2e18) break;
+                x *= i;
+                if (e >= 3 && e % 2 == 1) {
+                    long sr = sqrt(x);
+                    if (sr * sr != x) {
+                        all[p++] = x;
+                    }
                 }
             }
         }
+        all = Arrays.copyOf(all, p);
+        Arrays.sort(all);
+        all = uniq(all);
 
-        return set.size();
+        for (int Q = ni(); Q > 0; Q--) {
+            long L = nl(), R = nl();
+            long ans = 0;
+            long sr = sqrt(R);
+            long sl = sqrt(L - 1);
+            ans += sr - sl;
+            int rr = Arrays.binarySearch(all, R);
+            int ll = Arrays.binarySearch(all, L - 1);
+            if (rr < 0) rr = -rr - 2;
+            if (ll < 0) ll = -ll - 2;
+            ans += rr - ll;
+            out.println(ans);
+        }
     }
 
-    public static long fast_pow(long a, long b) {
-        long result = 1;
-        while (b > 0) {
-            if (b % 2 != 0) {
-                result *= a;
-                b--;
+    public static long[] uniq(long[] a) {
+        int n = a.length;
+        int p = 0;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || a[i] != a[i - 1]) a[p++] = a[i];
+        }
+        return Arrays.copyOf(a, p);
+    }
+
+
+    public static long sqrt(long n) {
+        long i = Math.max(0, (long) Math.sqrt(n) - 2);
+        while (i * i <= n) i++;
+        return i - 1;
+    }
+
+
+    void run() throws Exception {
+        is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+        out = new PrintWriter(System.out);
+
+        long s = System.currentTimeMillis();
+        solve();
+        out.flush();
+        tr(System.currentTimeMillis() - s + "ms");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new C().run();
+    }
+
+    private byte[] inbuf = new byte[1024];
+    public int lenbuf = 0, ptrbuf = 0;
+
+    private int readByte() {
+        if (lenbuf == -1) throw new InputMismatchException();
+        if (ptrbuf >= lenbuf) {
+            ptrbuf = 0;
+            try {
+                lenbuf = is.read(inbuf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
             }
-            a *= a;
-            b /= 2;
+            if (lenbuf <= 0) return -1;
         }
-
-        return result;
-
+        return inbuf[ptrbuf++];
     }
 
+    private boolean isSpaceChar(int c) {
+        return !(c >= 33 && c <= 126);
+    }
 
-    public static class FastScanner {
-        BufferedReader br;
-        StringTokenizer st;
+    private int skip() {
+        int b;
+        while ((b = readByte()) != -1 && isSpaceChar(b)) ;
+        return b;
+    }
 
-        public FastScanner(InputStream i) {
-            br = new BufferedReader(new InputStreamReader(i));
-            st = null;
+    private double nd() {
+        return Double.parseDouble(ns());
+    }
+
+    private char nc() {
+        return (char) skip();
+    }
+
+    private String ns() {
+        int b = skip();
+        StringBuilder sb = new StringBuilder();
+        while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b != ' ')
+            sb.appendCodePoint(b);
+            b = readByte();
+        }
+        return sb.toString();
+    }
+
+    private char[] ns(int n) {
+        char[] buf = new char[n];
+        int b = skip(), p = 0;
+        while (p < n && !(isSpaceChar(b))) {
+            buf[p++] = (char) b;
+            b = readByte();
+        }
+        return n == p ? buf : Arrays.copyOf(buf, p);
+    }
+
+    private char[][] nm(int n, int m) {
+        char[][] map = new char[n][];
+        for (int i = 0; i < n; i++) map[i] = ns(m);
+        return map;
+    }
+
+    private int[] na(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = ni();
+        return a;
+    }
+
+    private int ni() {
+        int num = 0, b;
+        boolean minus = false;
+        while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-')) ;
+        if (b == '-') {
+            minus = true;
+            b = readByte();
         }
 
-        public String next() throws IOException {
-            while (st == null || !st.hasMoreTokens())
-                st = new StringTokenizer(br.readLine());
-            return st.nextToken();
-        }
-
-        public String nextLine() throws IOException {
-            if (st == null)
-                return br.readLine();
-            String line = "";
-            if (st.hasMoreTokens())
-                line = st.nextToken("");
-            st = null;
-            return line;
-        }
-
-        public int nextInt() throws IOException {
-            return Integer.parseInt(next());
-        }
-
-        public long nextLong() throws IOException {
-            return Long.parseLong(next());
-        }
-
-        public double nextDouble() throws IOException {
-            return Double.parseDouble(next());
+        while (true) {
+            if (b >= '0' && b <= '9') {
+                num = num * 10 + (b - '0');
+            } else {
+                return minus ? -num : num;
+            }
+            b = readByte();
         }
     }
 
+    private long nl() {
+        long num = 0;
+        int b;
+        boolean minus = false;
+        while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-')) ;
+        if (b == '-') {
+            minus = true;
+            b = readByte();
+        }
+
+        while (true) {
+            if (b >= '0' && b <= '9') {
+                num = num * 10 + (b - '0');
+            } else {
+                return minus ? -num : num;
+            }
+            b = readByte();
+        }
+    }
+
+    private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+
+    private void tr(Object... o) {
+        if (!oj) System.out.println(Arrays.deepToString(o));
+    }
 }
