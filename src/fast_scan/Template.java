b86781c740b29c76df36195ff8819995311c7000
prituladima <prituladima@gmail.com>
2018-04-03 10:58:22 +0300

    predefined

---
+++ src/fast_scan/Template.java
@@ -0,0 +1,54 @@
+package fast_scan;
+
+import java.io.*;
+import java.util.*;
+
+public class Template {
+
+    private void solve() throws IOException {
+
+        //solution
+
+    }
+
+    public static void main(String[] args) {
+        new Template().run();
+    }
+
+    BufferedReader reader;
+    StringTokenizer tokenizer;
+    PrintWriter writer;
+
+    public void run() {
+        try {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = null;
+            writer = new PrintWriter(System.out);
+            solve();
+            reader.close();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    int nextInt() throws IOException {
+        return Integer.parseInt(nextToken());
+    }
+
+    long nextLong() throws IOException {
+        return Long.parseLong(nextToken());
+    }
+
+    double nextDouble() throws IOException {
+        return Double.parseDouble(nextToken());
+    }
+
+    String nextToken() throws IOException {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            tokenizer = new StringTokenizer(reader.readLine());
+        }
+        return tokenizer.nextToken();
+    }
+}
