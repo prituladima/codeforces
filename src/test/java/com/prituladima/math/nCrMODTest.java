2c356ec54503e0c8ec1943f30efdae21ac14b5a0
prituladima <prituladima@gmail.com>
2018-06-09 18:53:32 +0300

    ---

--- src/test/java/com/prituladima/math/nCrMODTest.java
+++ src/test/java/com/prituladima/math/nCrMODTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(value = Parameterized.class)
 public class nCrMODTest {
 
-    final nCrMOD nCr = new nCrMOD();
+    final nCrMOD nCr_mod = new nCrMOD();
     private static final int MOD = 1000000007;
 
     private final int modulo;
@@ -26,7 +26,7 @@ public class nCrMODTest {
         this.expected = expected;
     }
 
-    @Parameterized.Parameters(name = "{index}: nCr({0}, {1}, {2})={3}")
+    @Parameterized.Parameters(name = "{index}: nCr_mod({0}, {1}, {2})={3}")
     public static Iterable<Object[]> data() {
         return Arrays.asList(new Object[][]{
                         {794, 736, MOD, 927517870},
@@ -37,8 +37,8 @@ public class nCrMODTest {
 
     @Test(timeout = 2000)
     public void test() throws Exception {
-        assertEquals(expected, nCr.binomialCoeff_dp_opt(n, r));
-        assertEquals(expected, nCr.binomialCoeff_dp(n, r));
+        assertEquals(expected, nCr_mod.binomialCoeff_dp_opt(n, r));
+        assertEquals(expected, nCr_mod.binomialCoeff_dp(n, r));
     }
 
 
