39013dcc6812a9a2d783bc4db563615fc4b79e87
prituladima <prituladima@gmail.com>
2018-06-23 12:04:08 +0300

    A - Mishka and Contest

---
+++ src/main/java/com/prituladima/codeforce/contests/contest999/A.java
@@ -0,0 +1,104 @@
+package com.prituladima.codeforce.contests.contest999;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * Created by prituladima on 6/23/18.
+ */
+public class A {
+
+    private void solve() throws IOException {
+
+        int n = nextInt(), k = nextInt();
+        int[] a = nextArr(n);
+
+        int ans = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] <= k){
+                if(i == n - 1){
+                    System.out.println(n);
+                    return;
+                }
+                ans++;
+            } else break;
+        }
+
+        for (int i = a.length - 1; i >= 0; i--) {
+            if(a[i] <= k){
+                ans++;
+            }else break;
+        }
+
+        System.out.println(ans);
+
+
+    }
+
+    public static void main(String[] args) {
+        new A().run();
+    }
+
+    StringTokenizer tokenizer;
+    BufferedReader reader;
+    PrintWriter writer;
+
+    public void run() {
+        try {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = null;
+            writer = new PrintWriter(System.out);
+            solve();
+            reader.close();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    int nextInt() throws IOException {
+        return Integer.parseInt(nextToken());
+    }
+
+    long nextLong() throws IOException {
+        return Long.parseLong(nextToken());
+    }
+
+    double nextDouble() throws IOException {
+        return Double.parseDouble(nextToken());
+    }
+
+    String nextToken() throws IOException {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            tokenizer = new StringTokenizer(reader.readLine());
+        }
+        return tokenizer.nextToken();
+    }
+
+    int[] nextArr(int size) throws NumberFormatException, IOException {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++)
+            arr[i] = nextInt();
+        return arr;
+    }
+
+    long[] nextArrL(int size) throws NumberFormatException, IOException {
+        long[] arr = new long[size];
+        for (int i = 0; i < size; i++)
+            arr[i] = nextLong();
+        return arr;
+    }
+
+    double[] nextArrD(int size) throws NumberFormatException, IOException {
+        double[] arr = new double[size];
+        for (int i = 0; i < size; i++)
+            arr[i] = nextDouble();
+        return arr;
+    }
+
+}
