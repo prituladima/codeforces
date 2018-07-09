be4fda1527aae548a3a7cd53f39d16150fe31ba3
Dmytro_Prytula <prituladima@gmail.com>
2018-07-09 17:12:31 +0300

    Template Ladder Name: 5 - Codeforces Div. 2, A

---
+++ src/main/java/com/prituladima/codeforce/a2oj/div2B/Pro25.java
@@ -0,0 +1,122 @@
+package com.prituladima.codeforce.a2oj.div2B;
+
+
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.util.Arrays.stream;
+import static java.util.stream.IntStream.range;
+
+/**
+ * Created by prituladima on 5/23/18.
+ */
+public final class Pro25 {
+
+    private void solve() {
+
+        //put your code here
+
+    }
+
+    public static void main(String[] args) {
+        new Pro25().run();
+    }
+
+    private BufferedReader reader;
+    private StringTokenizer tokenizer;
+    private PrintWriter writer;
+
+    private void run() {
+        try {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = null;
+            writer = new PrintWriter(System.out);
+            solve();
+            reader.close();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private int nextInt() {
+        return parseInt(nextToken());
+    }
+
+    private long nextLong() {
+        return parseLong(nextToken());
+    }
+
+    private double nextDouble() {
+        return parseDouble(nextToken());
+    }
+
+    private int[] nextArr(int size) {
+        return stream(new int[size]).map(c -> nextInt()).toArray();
+    }
+
+    private long[] nextArrL(int size) {
+        return stream(new long[size]).map(c -> nextLong()).toArray();
+    }
+
+    private double[] nextArrD(int size) {
+        return stream(new double[size]).map(c -> nextDouble()).toArray();
+    }
+
+    private char[][] nextCharMatrix(int n) {
+        return range(0, n).mapToObj(i -> nextToken().toCharArray()).toArray(char[][]::new);
+    }
+
+    private int[][] nextIntMatrix(final int n, final int m) {
+        return range(0, n).mapToObj(i -> nextArr(m)).toArray(int[][]::new);
+    }
+
+    private double[][] nextDoubleMatrix(int n, int m) {
+        return range(0, n).mapToObj(i -> nextArr(m)).toArray(double[][]::new);
+    }
+
+    private String nextToken() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+
+    private void souf(String format, Object... args) {
+        writer.printf(format, args);
+    }
+
+    private void sout(Object o) {
+        writer.print(o);
+    }
+
+    private void newLine() {
+        writer.println();
+    }
+
+    private void soutnl(Object o) {
+        sout(o);
+        newLine();
+    }
+
+    private void soutCharArray(char[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            sout(arr[i]);
+        }
+    }
+
+    private <T extends Number> void soutNumberArray(T[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            soutnl(arr[i]);
+        }
+    }
+
+}
