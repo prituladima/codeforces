6b93d5b52dbadcc182a23e67c492c6a44f12ca87
prituladima <prituladima@gmail.com>
2018-07-18 21:19:36 +0300

    [a2oj] 4 - Codeforces Div. 2, A 96

--- src/main/java/com/prituladima/codeforce/a2oj/div2A/Pro96.java
+++ src/main/java/com/prituladima/codeforce/a2oj/div2A/Pro96.java
@@ -1,7 +1,6 @@
 package com.prituladima.codeforce.a2oj.div2A;
 
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,14 +8,51 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+import static java.lang.Integer.max;
+import static java.lang.Integer.min;
+import static java.lang.Math.abs;
 import static java.util.stream.IntStream.range;
+
 
 public class Pro96 {
 
+    int n, m, i, j, a, b;
+    int INF = Integer.MAX_VALUE;
+
     private void solve() {
 
-        //put your code here
+        n = nextInt();
+        m = nextInt();
+        i = nextInt();
+        j = nextInt();
+        a = nextInt();
+        b = nextInt();
+        int ans = INF;
 
+        ans = min(check(1, m), check(n, 1));
+        ans = min(ans, check(n, m));
+        ans = min(ans, check(1, 1));
+
+
+        sout(ans == Integer.MAX_VALUE ? "Poor Inna and pony!" : ans);
+
+    }
+
+
+    int check(int x, int y) {
+        if (x == i && y == j) {
+            return 0;
+        }
+        if (i - a < 1 && i + a > n) return INF;
+        if (j - b < 1 && j + b > m) return INF;
+        if (abs(x - i) % a != 0) return INF;
+        if (abs(y - j) % b != 0) return INF;
+
+        int cost = 0;
+        cost = max(abs(x - i) / a, abs(y - j) / b);
+        int tmp = abs(x - i) / a + abs(y - j) / b;
+        if ((tmp & 1) != 0) return INF;
+        return cost;
     }
 
     public static void main(String[] args) {
