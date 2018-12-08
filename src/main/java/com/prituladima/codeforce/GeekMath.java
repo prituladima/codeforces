610037a3b354db09ccf4da79a6c954ae6905c299
prituladima <prituladima@gmail.com>
2018-12-08 20:00:04 +0200

    Gradle wrapper 4.10.2 version Java 11 support

---
+++ src/main/java/com/prituladima/codeforce/GeekMath.java
@@ -0,0 +1,9 @@
+package com.prituladima.codeforce;
+
+public class GeekMath {
+
+    public static long ceiling(long n, long k) {
+        return (n + k - 1) / k;
+    }
+
+}
--- src/main/java/com/prituladima/codeforce/contests/contest1036/A.java
+++ src/main/java/com/prituladima/codeforce/contests/contest1036/A.java
@@ -1,13 +1,60 @@
-package com.prituladima.codeforce.contests.contest1036;
+//package com.prituladima.codeforce.contests.contest1036;
 
-import com.prituladima.codeforce.InputReader;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.PrintWriter;
 
 public class A {
-    public void solve(int testNumber, InputReader in, PrintWriter out) {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        MAin solver = new MAin();
+        solver.solve(1, in, out);
+        out.close();
+    }
 
-        long k = in.nextLong(), n = in.nextLong();
-        out.print((n + k - 1)/k);
+    static class MAin {
+        public void solve(int testNumber, InputReader in, PrintWriter out) {
+
+            long k = in.nextLong(), n = in.nextLong();
+            out.print((n + k -1)/k);
+
+        }
+
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String nextToken() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextToken());
+        }
 
     }
 }
