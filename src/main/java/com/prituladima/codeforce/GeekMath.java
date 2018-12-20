0b2557d9f61e081fd6ae1b0567b61b0bdffd6416
prituladima <prituladima@gmail.com>
2018-12-20 02:43:14 +0200

    https://codeforces.com/contest/1082/submission/47280394

--- src/main/java/com/prituladima/codeforce/GeekMath.java
+++ src/main/java/com/prituladima/codeforce/GeekMath.java
@@ -1,5 +1,8 @@
 package com.prituladima.codeforce;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class GeekMath {
 
     public static long ceiling(long n, long k) {
@@ -34,4 +37,31 @@ public class GeekMath {
         }
         return low;
     }
+
+
+    long[] shuffle(long[] arr) {
+        Random r = new Random();
+        for (int i = 1, j; i < arr.length; i++) {
+            j = r.nextInt(i);
+            arr[i] = arr[i] ^ arr[j];
+            arr[j] = arr[i] ^ arr[j];
+            arr[i] = arr[i] ^ arr[j];
+        }
+        return arr;
+    }
+
+    int[] uniq(int[] arr) {
+        Arrays.sort(arr);
+        int[] rv = new int[arr.length];
+        int pos = 0;
+        rv[pos++] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                rv[pos++] = arr[i];
+            }
+        }
+        return Arrays.copyOf(rv, pos);
+    }
+
+
 }
--- src/main/java/com/prituladima/codeforce/contests/contest1082/C.java
+++ src/main/java/com/prituladima/codeforce/contests/contest1082/C.java
@@ -1,113 +1,256 @@
 package com.prituladima.codeforce.contests.contest1082;
-import java.io.*;
+
+import java.io.PrintWriter;
 import java.util.*;
+import java.io.*;
 
-import static java.lang.Double.parseDouble;
-import static java.lang.Integer.parseInt;
-import static java.lang.Long.parseLong;
-import static java.util.Arrays.stream;
-import static java.util.stream.IntStream.range;
 
-public final class C {
+public class C implements Runnable {
 
-    private void solve() {
+    FastReader scn;
+    PrintWriter out;
+    String INPUT = "";
 
-        //put your code here
+    void solve() {
+        int n = scn.nextInt(), m = scn.nextInt();
+        int[][] arr = new int[n][2];
+        int[] count = new int[m + 1];
+        for (int i = 0; i < n; i++) {
+            int x = scn.nextInt(), y = scn.nextInt();
+            arr[i][0] = x;
+            arr[i][1] = y;
+            count[x]++;
+        }
 
+        long[][] need = new long[m + 1][];
+        for (int i = 0; i <= m; i++) {
+            need[i] = new long[count[i]];
+        }
+
+        for(int i = 0; i < n; i++) {
+            need[arr[i][0]][--count[arr[i][0]]] = -arr[i][1];
+        }
+
+        for(int i = 0; i <= m; i++) {
+            need[i] = scn.shuffle(need[i]);
+            Arrays.sort(need[i]);
+
+            for(int j = 0; j < need[i].length; j++) {
+                need[i][j] *= -1;
+            }
+
+            for(int j = 1; j < need[i].length; j++) {
+                need[i][j] += need[i][j - 1];
+            }
+        }
+
+        long[] ans = new long[Math.max(n , m)];
+        for(int i = 0; i <= m; i++) {
+            for(int j = 0; j < need[i].length; j++) {
+                if(need[i][j] >= 0) {
+                    ans[j] += need[i][j];
+                }
+            }
+        }
+        long max = 0;
+        for(long z : ans) {
+            max = Math.max(max, z);
+        }
+        out.println(max);
+    }
+
+    public void run() {
+        long time = System.currentTimeMillis();
+        boolean oj = true;//System.getProperty("ONLINE_JUDGE") != null;
+        out = new PrintWriter(System.out);
+        scn = new FastReader(oj);
+        solve();
+        out.flush();
+        if (!oj) {
+            System.out.println(Arrays.deepToString(new Object[] { System.currentTimeMillis() - time + " ms" }));
+        }
     }
 
     public static void main(String[] args) {
-        new C().run();
+        new Thread(null, new C(), "Main", 1 << 26).start();
     }
 
-    private BufferedReader reader;
-    private StringTokenizer tokenizer;
-    private PrintWriter writer;
+    class FastReader {
+        InputStream is;
 
-    private void run() {
-        try {
-            reader = new BufferedReader(new InputStreamReader(System.in));
-            tokenizer = null;
-            writer = new PrintWriter(System.out);
-            solve();
-            reader.close();
-            writer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
+        public FastReader(boolean onlineJudge) {
+            is = onlineJudge ? System.in : new ByteArrayInputStream(INPUT.getBytes());
         }
-    }
 
-    private int nextInt() {
-        return parseInt(nextToken());
-    }
+        byte[] inbuf = new byte[1024];
+        public int lenbuf = 0, ptrbuf = 0;
 
-    private long nextLong() {
-        return parseLong(nextToken());
-    }
+        int readByte() {
+            if (lenbuf == -1)
+                throw new InputMismatchException();
+            if (ptrbuf >= lenbuf) {
+                ptrbuf = 0;
+                try {
+                    lenbuf = is.read(inbuf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (lenbuf <= 0)
+                    return -1;
+            }
+            return inbuf[ptrbuf++];
+        }
 
-    private double nextDouble() {
-        return parseDouble(nextToken());
-    }
+        boolean isSpaceChar(int c) {
+            return !(c >= 33 && c <= 126);
+        }
 
-    private int[] nextArr(int size) {
-        return stream(new int[size]).map(c -> nextInt()).toArray();
-    }
+        int skip() {
+            int b;
+            while ((b = readByte()) != -1 && isSpaceChar(b))
+                ;
+            return b;
+        }
 
-    private long[] nextArrL(int size) {
-        return stream(new long[size]).map(c -> nextLong()).toArray();
-    }
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
 
-    private double[] nextArrD(int size) {
-        return stream(new double[size]).map(c -> nextDouble()).toArray();
-    }
+        char nextChar() {
+            return (char) skip();
+        }
 
-    private char[][] nextCharMatrix(int n) {
-        return range(0, n).mapToObj(i -> nextToken().toCharArray()).toArray(char[][]::new);
-    }
+        String next() {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b != ' ')
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
 
-    private int[][] nextIntMatrix(final int n, final int m) {
-        return range(0, n).mapToObj(i -> nextArr(m)).toArray(int[][]::new);
-    }
+        String nextLine() {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while ((!isSpaceChar(b) || b == ' ')) { // when nextLine, (isSpaceChar(b) && b != ' ')
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
 
-    private double[][] nextDoubleMatrix(int n, int m) {
-        return range(0, n).mapToObj(i -> nextArr(m)).toArray(double[][]::new);
-    }
+        char[] next(int n) {
+            char[] buf = new char[n];
+            int b = skip(), p = 0;
+            while (p < n && !(isSpaceChar(b))) {
+                buf[p++] = (char) b;
+                b = readByte();
+            }
+            return n == p ? buf : Arrays.copyOf(buf, p);
+        }
 
-    private String nextToken() {
-        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-            try {
-                tokenizer = new StringTokenizer(reader.readLine());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        int nextInt() {
+            int num = 0, b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
             }
         }
-        return tokenizer.nextToken();
-    }
 
-    private void souf(String format, Object... args) {
-        writer.printf(format, args);
-    }
+        long nextLong() {
+            long num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
 
-    private void sout(Object o) {
-        writer.print(o);
-    }
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
 
-    private void newLine() {
-        writer.println();
-    }
+        char[][] nextMatrix(int n, int m) {
+            char[][] map = new char[n][];
+            for (int i = 0; i < n; i++)
+                map[i] = next(m);
+            return map;
+        }
 
-    private void soutnl(Object o) {
-        sout(o);
-        newLine();
-    }
+        int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
 
-    private int max(int a, int b) {
-        return Math.max(a, b);
-    }
+        long[] nextLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextLong();
+            return a;
+        }
 
-    private int min(int a, int b) {
-        return Math.min(a, b);
+        int[][] next2DInt(int n, int m) {
+            int[][] arr = new int[n][];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextIntArray(m);
+            }
+            return arr;
+        }
+
+        long[][] next2DLong(int n, int m) {
+            long[][] arr = new long[n][];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLongArray(m);
+            }
+            return arr;
+        }
+
+        long[] shuffle(long[] arr) {
+            Random r = new Random();
+            for (int i = 1, j; i < arr.length; i++) {
+                j = r.nextInt(i);
+                arr[i] = arr[i] ^ arr[j];
+                arr[j] = arr[i] ^ arr[j];
+                arr[i] = arr[i] ^ arr[j];
+            }
+            return arr;
+        }
+
+        int[] uniq(int[] arr) {
+            Arrays.sort(arr);
+            int[] rv = new int[arr.length];
+            int pos = 0;
+            rv[pos++] = arr[0];
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] != arr[i - 1]) {
+                    rv[pos++] = arr[i];
+                }
+            }
+            return Arrays.copyOf(rv, pos);
+        }
     }
 
 }
