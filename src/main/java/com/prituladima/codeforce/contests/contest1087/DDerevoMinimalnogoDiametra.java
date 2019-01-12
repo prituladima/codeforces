df376068c1da92eff375953637b7d3b83d5a8669
Dmytro Prytula <prituladima@gmail.com>
2019-01-13 01:07:00 +0200

    https://codeforces.com/blog/entry/64078

--- src/main/java/com/prituladima/codeforce/contests/contest1087/DDerevoMinimalnogoDiametra.java
+++ src/main/java/com/prituladima/codeforce/contests/contest1087/DDerevoMinimalnogoDiametra.java
@@ -1,31 +1,26 @@
 package com.prituladima.codeforce.contests.contest1087;
 
+import com.prituladima.codeforce.GeekInteger;
 import com.prituladima.codeforce.InputReader;
+
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class DDerevoMinimalnogoDiametra {
 
-    private static class Edge{
-        int first;
-        int second;
-
-        public Edge(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-    }
-
     public void solve(int testNumber, InputReader in, PrintWriter out) {
-
         int n = in.nextInt(), s = in.nextInt();
-        List<Edge> list = new ArrayList<>();
-        for (int i = 0; i < n - 1; i++) {
-            list.add(new Edge(in.nextInt(), in.nextInt()));
+        int[] a = new int[2*(n -1)];
+        for (int i = 0; i < 2*(n - 1); i++) {
+            a[i] = in.nextInt();
         }
-
-
-
+        Map<Integer, Integer> ms = GeekInteger.multiSet(a);
+        int ans = 0;
+        for (int i : ms.keySet()) {
+            ans += (ms.get(i) == 1) ? 1 : 0;
+        }
+        out.print(2*(double)s/(double) ans);
     }
 }
