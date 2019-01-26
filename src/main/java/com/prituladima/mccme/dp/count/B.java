814ac8954224543a3c766dfdace8b1f98377652d
Dmytro Prytula <prituladima@gmail.com>
2019-01-26 15:46:11 +0200

    https://informatics.mccme.ru/mod/statements/view.php?id=2499#1

---
+++ src/main/java/com/prituladima/mccme/dp/count/B.java
@@ -0,0 +1,127 @@
+package com.prituladima.mccme.dp.count;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.util.Arrays.stream;
+import static java.util.stream.IntStream.range;
+
+public final class B {
+
+    private void solve() {
+
+        int n = nextInt();
+        int[][] dp = new int[n + 1][3];
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+        dp[1][2] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2];
+            dp[i][1] = dp[i - 1][0] + dp[i - 1][2];
+            dp[i][2] = dp[i][0];
+        }
+
+        sout(dp[n][0] + dp[n][1] + dp[n][2]);
+    }
+
+    public static void main(String[] args) {
+        new B().run();
+    }
+
+    private BufferedReader reader;
+    private StringTokenizer tokenizer;
+    private PrintWriter writer;
+
+    private void run() {
+        try {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = null;
+            writer = new PrintWriter(System.out);
+            solve();
+            reader.close();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private int nextInt() {
+        return parseInt(nextToken());
+    }
+
+    private long nextLong() {
+        return parseLong(nextToken());
+    }
+
+    private double nextDouble() {
+        return parseDouble(nextToken());
+    }
+
+    private int[] nextArr(int size) {
+        return stream(new int[size]).map(c -> nextInt()).toArray();
+    }
+
+    private long[] nextArrL(int size) {
+        return stream(new long[size]).map(c -> nextLong()).toArray();
+    }
+
+    private double[] nextArrD(int size) {
+        return stream(new double[size]).map(c -> nextDouble()).toArray();
+    }
+
+    private char[][] nextCharMatrix(int n) {
+        return range(0, n).mapToObj(i -> nextToken().toCharArray()).toArray(char[][]::new);
+    }
+
+    private int[][] nextIntMatrix(final int n, final int m) {
+        return range(0, n).mapToObj(i -> nextArr(m)).toArray(int[][]::new);
+    }
+
+    private double[][] nextDoubleMatrix(int n, int m) {
+        return range(0, n).mapToObj(i -> nextArr(m)).toArray(double[][]::new);
+    }
+
+    private String nextToken() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+
+    private void souf(String format, Object... args) {
+        writer.printf(format, args);
+    }
+
+    private void sout(Object o) {
+        writer.print(o);
+    }
+
+    private void newLine() {
+        writer.println();
+    }
+
+    private void soutnl(Object o) {
+        sout(o);
+        newLine();
+    }
+
+    private int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    private int min(int a, int b) {
+        return Math.min(a, b);
+    }
+
+}
+
---
+++ src/main/java/com/prituladima/mccme/dp/count/I.java
@@ -0,0 +1,149 @@
+package com.prituladima.mccme.dp.count;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.util.Arrays.fill;
+import static java.util.Arrays.stream;
+import static java.util.stream.IntStream.range;
+
+public final class I {
+
+    private void solve() {
+        int n = nextInt();
+
+        int[][] steps = {
+                {4, 6},
+                {6, 8},
+                {7, 9},
+                {4, 8},
+                {3, 9, 0},
+
+                {},
+                {1, 7, 0},
+                {2, 6},
+                {1, 3},
+                {2, 4}
+        };
+
+
+        int[][] dp = new int[n + 1][10];
+        fill(dp[1], 1);
+        dp[1][0] = 0;
+        dp[1][8] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k : steps[j])
+                    dp[i][j] += dp[i - 1][k];
+            }
+        }
+        int ans = 0;
+        for (int j = 0; j < 10; j++) {
+            ans += dp[n][j];
+        }
+        sout(ans);
+    }
+
+    public static void main(String[] args) {
+        new I().run();
+    }
+
+    private BufferedReader reader;
+    private StringTokenizer tokenizer;
+    private PrintWriter writer;
+
+    private void run() {
+        try {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            tokenizer = null;
+            writer = new PrintWriter(System.out);
+            solve();
+            reader.close();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private int nextInt() {
+        return parseInt(nextToken());
+    }
+
+    private long nextLong() {
+        return parseLong(nextToken());
+    }
+
+    private double nextDouble() {
+        return parseDouble(nextToken());
+    }
+
+    private int[] nextArr(int size) {
+        return stream(new int[size]).map(c -> nextInt()).toArray();
+    }
+
+    private long[] nextArrL(int size) {
+        return stream(new long[size]).map(c -> nextLong()).toArray();
+    }
+
+    private double[] nextArrD(int size) {
+        return stream(new double[size]).map(c -> nextDouble()).toArray();
+    }
+
+    private char[][] nextCharMatrix(int n) {
+        return range(0, n).mapToObj(i -> nextToken().toCharArray()).toArray(char[][]::new);
+    }
+
+    private int[][] nextIntMatrix(final int n, final int m) {
+        return range(0, n).mapToObj(i -> nextArr(m)).toArray(int[][]::new);
+    }
+
+    private double[][] nextDoubleMatrix(int n, int m) {
+        return range(0, n).mapToObj(i -> nextArr(m)).toArray(double[][]::new);
+    }
+
+    private String nextToken() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+
+    private void souf(String format, Object... args) {
+        writer.printf(format, args);
+    }
+
+    private void sout(Object o) {
+        writer.print(o);
+    }
+
+    private void newLine() {
+        writer.println();
+    }
+
+    private void soutnl(Object o) {
+        sout(o);
+        newLine();
+    }
+
+    private int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    private int min(int a, int b) {
+        return Math.min(a, b);
+    }
+
+}
+
