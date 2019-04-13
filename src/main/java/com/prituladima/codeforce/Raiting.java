93edd6573f27fdf6cfa97c8404bd62cc2aa57c95
Dmytro Prytula <prituladima@gmail.com>
2019-04-13 21:42:45 +0300

    -

---
+++ src/main/java/com/prituladima/codeforce/Raiting.java
@@ -0,0 +1,132 @@
+package com.prituladima.codeforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.util.Arrays.stream;
+import static java.util.stream.IntStream.range;
+
+public final class Raiting {
+
+    private void solve() {
+        int len = 40;
+        double cur = 1346;
+        for (int i = 0; i <= 3000; i += 100) {
+            double prob = 1.0 / (1 + (Math.pow(10, (i - cur) / 400)));
+            char[] chars = new char[len];
+            Arrays.fill(chars, ' ');
+            for (int j = 0; j < len * prob; j++) {
+                chars[j] = '-';
+            }
+            chars[0] = '[';
+            chars[len - 1] = ']';
+            sout("" + String.format("%4d", i) + " - " + String.valueOf(chars) + "\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Raiting().run();
+    }
+
+    private BufferedReader reader;
+    private StringTokenizer tokenizer;
+    private PrintWriter writer;
+
+    private void run() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter writer = new PrintWriter(System.out)) {
+            this.reader = reader;
+            this.writer = writer;
+            this.tokenizer = null;
+            solve();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+
+    private int nextInt(int radix) {
+        return parseInt(nextToken(), radix);
+    }
+
+    private int nextInt() {
+        return parseInt(nextToken());
+    }
+
+    private long nextLong(int radix) {
+        return parseLong(nextToken(), radix);
+    }
+
+    private long nextLong() {
+        return parseLong(nextToken());
+    }
+
+    private double nextDouble() {
+        return parseDouble(nextToken());
+    }
+
+    private int[] nextArr(int size) {
+        return stream(new int[size]).map(c -> nextInt()).toArray();
+    }
+
+    private long[] nextArrL(int size) {
+        return stream(new long[size]).map(c -> nextLong()).toArray();
+    }
+
+    private double[] nextArrD(int size) {
+        return stream(new double[size]).map(c -> nextDouble()).toArray();
+    }
+
+    private char[][] nextCharMatrix(int n) {
+        return range(0, n).mapToObj(i -> nextToken().toCharArray()).toArray(char[][]::new);
+    }
+
+    private int[][] nextIntMatrix(final int n, final int m) {
+        return range(0, n).mapToObj(i -> nextArr(m)).toArray(int[][]::new);
+    }
+
+    private double[][] nextDoubleMatrix(final int n, final int m) {
+        return range(0, n).mapToObj(i -> nextArrD(m)).toArray(double[][]::new);
+    }
+
+    private String nextToken() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+
+    private void souf(String format, Object... args) {
+        writer.printf(format, args);
+    }
+
+    private void sout(Object o) {
+        writer.print(o);
+    }
+
+    private void newLine() {
+        writer.println();
+    }
+
+    private void soutnl(Object o) {
+        sout(o);
+        newLine();
+    }
+
+    private void flush() {
+        writer.flush();
+    }
+
+}
