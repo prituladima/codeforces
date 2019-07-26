6dfa86bb98634d421051242f91646666adf708b8
Dmytro Prytula <dmytro_prytula@epam.com>
2019-07-26 17:54:50 +0300

    -

---
+++ src/main/java/com/prituladima/google/prep/N0001_Find_sum_of_different_corresponding_bits_for_all_pairs.java
@@ -0,0 +1,135 @@
+package com.prituladima.google.prep;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.util.Arrays.stream;
+import static java.util.stream.IntStream.range;
+
+/**
+ * https://practice.geeksforgeeks.org/problems/find-sum-of-different-corresponding-bits-for-all-pairs/0
+ */
+public class N0001_Find_sum_of_different_corresponding_bits_for_all_pairs {
+
+    private void solve() {
+        final int MOD = (int)1e9 + 7;
+        int T = nextInt();
+        while (T-- > 0){
+            int N = nextInt();
+            int[] A = nextArr(N);
+            int sum = 0;
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    int xor = A[i] ^ A[j];
+                    int localSum = Integer.bitCount(xor);
+                    sum += localSum;
+                    sum %= MOD;
+                }
+            }
+            soutnl(sum);
+        }
+    }
+
+    public static void main(String[] args) {
+        new N0001_Find_sum_of_different_corresponding_bits_for_all_pairs().run();
+    }
+
+    private BufferedReader reader;
+    private StringTokenizer tokenizer;
+    private PrintWriter writer;
+
+    private void run() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out))) {
+            this.reader = reader;
+            this.writer = writer;
+            this.tokenizer = null;
+            solve();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+
+    private int nextInt(int radix) {
+        return parseInt(nextToken(), radix);
+    }
+
+    private int nextInt() {
+        return parseInt(nextToken());
+    }
+
+    private long nextLong(int radix) {
+        return parseLong(nextToken(), radix);
+    }
+
+    private long nextLong() {
+        return parseLong(nextToken());
+    }
+
+    private double nextDouble() {
+        return parseDouble(nextToken());
+    }
+
+    private int[] nextArr(int size) {
+        return stream(new int[size]).map(c -> nextInt()).toArray();
+    }
+
+    private long[] nextArrL(int size) {
+        return stream(new long[size]).map(c -> nextLong()).toArray();
+    }
+
+    private double[] nextArrD(int size) {
+        return stream(new double[size]).map(c -> nextDouble()).toArray();
+    }
+
+    private char[][] nextCharMatrix(int n) {
+        return range(0, n).mapToObj(i -> nextToken().toCharArray()).toArray(char[][]::new);
+    }
+
+    private int[][] nextIntMatrix(final int n, final int m) {
+        return range(0, n).mapToObj(i -> nextArr(m)).toArray(int[][]::new);
+    }
+
+    private double[][] nextDoubleMatrix(final int n, final int m) {
+        return range(0, n).mapToObj(i -> nextArrD(m)).toArray(double[][]::new);
+    }
+
+    private String nextToken() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+
+    private void souf(String format, Object... args) {
+        writer.printf(format, args);
+    }
+
+    private void sout(Object o) {
+        writer.print(o);
+    }
+
+    private void newLine() {
+        writer.println();
+    }
+
+    private void soutnl(Object o) {
+        sout(o);
+        newLine();
+    }
+
+    private void flush() {
+        writer.flush();
+    }
+
+
+}
