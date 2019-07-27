76da8214167aa1524c483d9063e57bf5270dcb6d
Dmytro Prytula <prituladima@gmail.com>
2019-07-27 20:52:36 +0300

    leetcode Google top

--- src/main/java/com/prituladima/google/leetcode/F003_number_of_icelands.java
+++ src/main/java/com/prituladima/google/leetcode/F003_number_of_icelands.java
@@ -1,6 +1,8 @@
 package com.prituladima.google.leetcode;
 
 import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 import static java.lang.Double.parseDouble;
@@ -19,6 +21,7 @@ public class F003_number_of_icelands {
         x = grid.length;
         if (x == 0) return 0;
         y = grid[0].length;
+//        used = new boolean[x][y];
         int ans = 0;
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
@@ -32,8 +35,8 @@ public class F003_number_of_icelands {
     }
 
     private void dfs(int i, int j, char[][] grid) {
-
-        grid[i][j] = '1';
+//        used[i][j] = true;
+        grid[i][j] = '0';
         if (i + 1 < x && grid[i + 1][j] == '1') dfs(i + 1, j, grid);
 
         if (0 <= i - 1 && grid[i - 1][j] == '1') dfs(i - 1, j, grid);
@@ -41,10 +44,116 @@ public class F003_number_of_icelands {
         if (j + 1 < y && grid[i][j + 1] == '1') dfs(i, j + 1, grid);
 
         if (0 <= j - 1 && grid[i][j - 1] == '1') dfs(i, j - 1, grid);
+
     }
 
-    private boolean isOn(int i, int j) {
-        return (0 <= i && i < x && 0 <= j && j < y);
+    private void bfs(int i, int j, char[][] grid) {
+        grid[i][j] = '0';
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(i * y + j);
+        while (!queue.isEmpty()) {
+            int id = queue.remove();
+            int row = id / y;
+            int col = id % y;
+            if (row + 1 < x && grid[row + 1][col] == '1') {
+                grid[row + 1][col] = '0';
+                queue.offer((row + 1) * y + col);
+            }
+
+            if (row - 1 >= 0 && grid[row - 1][col] == '1') {
+                grid[row - 1][col] = '0';
+                queue.offer((row - 1) * y + col);
+            }
+
+            if (col + 1 < y && grid[row][col + 1] == '1') {
+                grid[row][col + 1] = '0';
+                queue.offer((row) * y + col + 1);
+            }
+
+            if (col - 1 >= 0 && grid[row][col - 1] == '1') {
+                grid[row][col - 1] = '0';
+                queue.offer((row) * y + col - 1);
+            }
+        }
+    }
+
+    class UnionFind {
+        int count; // # of connected components
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(char[][] grid) { // for problem 200
+            count = 0;
+            int m = grid.length;
+            int n = grid[0].length;
+            parent = new int[m * n];
+            rank = new int[m * n];
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (grid[i][j] == '1') {
+                        parent[i * n + j] = i * n + j;
+                        ++count;
+                    }
+                    rank[i * n + j] = 0;
+                }
+            }
+        }
+
+        public int find(int i) { // path compression
+            if (parent[i] != i) parent[i] = find(parent[i]);
+            return parent[i];
+        }
+
+        public void union(int x, int y) { // union with rank
+            int rootx = find(x);
+            int rooty = find(y);
+            if (rootx != rooty) {
+                if (rank[rootx] > rank[rooty]) {
+                    parent[rooty] = rootx;
+                } else if (rank[rootx] < rank[rooty]) {
+                    parent[rootx] = rooty;
+                } else {
+                    parent[rooty] = rootx; rank[rootx] += 1;
+                }
+                --count;
+            }
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    public int numIslands2(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        UnionFind uf = new UnionFind(grid);
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    grid[r][c] = '0';
+                    if (r - 1 >= 0 && grid[r-1][c] == '1') {
+                        uf.union(r * nc + c, (r-1) * nc + c);
+                    }
+                    if (r + 1 < nr && grid[r+1][c] == '1') {
+                        uf.union(r * nc + c, (r+1) * nc + c);
+                    }
+                    if (c - 1 >= 0 && grid[r][c-1] == '1') {
+                        uf.union(r * nc + c, r * nc + c - 1);
+                    }
+                    if (c + 1 < nc && grid[r][c+1] == '1') {
+                        uf.union(r * nc + c, r * nc + c + 1);
+                    }
+                }
+            }
+        }
+
+        return uf.getCount();
     }
 
     ////////////////
