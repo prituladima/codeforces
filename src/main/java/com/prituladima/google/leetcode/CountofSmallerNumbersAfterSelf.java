5185bfebccc66dba2374e2e437b020170cab069d
Dmytro Prytula <dmytro_prytula@epam.com>
2019-09-26 13:41:12 +0300

    leetcode Google top

--- src/main/java/com/prituladima/google/leetcode/CountofSmallerNumbersAfterSelf.java
+++ src/main/java/com/prituladima/google/leetcode/CountofSmallerNumbersAfterSelf.java
@@ -1,34 +1,52 @@
 package com.prituladima.google.leetcode;
 
-import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CountofSmallerNumbersAfterSelf {
-    public List<Integer> countSmaller(int[] nums) {
+/**
+ * https://leetcode.com/problems/count-of-smaller-numbers-after-self/discuss/76630/Evolve-from-brute-force-to-optimal-a-review-of-all-solutions
+ * https://leetcode.com/problems/count-of-smaller-numbers-after-self/discuss/76752/15-ms-accepted-java-soln-using-mergesort
+ *
+ */
+// TODO: 30.08.2019  mergesort and Fenwick tree
+public class CountOfSmallerNumbersAfterSelf {
+    public static void main(String[] args) {
 
-        NavigableSet<Integer> tree = new TreeSet<>(
-                (x, y) -> {
-                    if (Objects.equals(x, y)) return 1;
-                    else return Integer.compare(x, y);
-                }
-        );
-        List<Integer> list = new ArrayList<>();
-        int n = nums.length;
-        for (int i = n - 1; i >= 0; i--) {
+        System.out.println(new CountOfSmallerNumbersAfterSelf().countSmaller(new int[]{5, 2, 6, 1}));
 
-            SortedSet<Integer> subTree = tree.subSet(Integer.MIN_VALUE, nums[i]);
-            list.add(subTree.size());
-            tree.add(nums[i]);
-        }
-
-        Collections.reverse(list);
-
-//        Arrays.binarySearch()
-
-        return list;
     }
 
-    private void start (long number, LocalDate date, short scv, String... args){
-        //your logic here
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> res = new ArrayList<>(nums.length);
+        List<Integer> index = new ArrayList<>(nums.length);
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res.add(0);
+            index.add(i);
+        }
+
+        countSmallerSub(nums, index, 1 << 31, res);
+
+        return res;
+    }
+
+    private void countSmallerSub(int[] nums, List<Integer> index, int mask, List<Integer> res) {
+        if (mask != 0 && index.size() > 1) {
+            List<Integer> lowGroup = new ArrayList<>(index.size());
+            List<Integer> highGroup = new ArrayList<>(index.size());
+
+            int high = Math.max(0, mask);
+            for (int ind : index) {
+                if ((nums[ind] & mask) == high) {
+                    res.set(ind, res.get(ind) + lowGroup.size());
+                    highGroup.add(ind);
+                } else {
+                    lowGroup.add(ind);
+                }
+            }
+
+            countSmallerSub(nums, lowGroup, mask >>> 1, res);
+            countSmallerSub(nums, highGroup, mask >>> 1, res);
+        }
     }
 }
