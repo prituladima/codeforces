075213abec0adeff29f76c17c3c19cc413b60542
Dmytro Prytula <prituladima@gmail.com>
2019-11-03 22:32:22 +0200

    ACMP - Initial commit

---
+++ archive/unsorted/2019.11/2019.11.03 - ACMP/PyatyuPyatDvadcatPyat.java
@@ -0,0 +1,11 @@
+package com.prituladima.codeforce;
+
+import com.prituladima.yaal.io.InputReader;
+import com.prituladima.yaal.io.OutputWriter;
+
+public class PyatyuPyatDvadcatPyat {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int i = in.nextInt();
+        out.println(i * i);
+    }
+}
