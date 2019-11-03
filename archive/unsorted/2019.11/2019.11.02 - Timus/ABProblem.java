5f46e86024155caae6ad7e929d938ccdf396f8b4
Dmytro Prytula <prituladima@gmail.com>
2019-11-03 20:53:12 +0200

    Timus - Initial commit

---
+++ archive/unsorted/2019.11/2019.11.02 - Timus/ABProblem.java
@@ -0,0 +1,13 @@
+package com.prituladima.codeforce;
+
+import com.prituladima.yaal.io.InputReader;
+import com.prituladima.yaal.io.OutputWriter;
+
+public class ABProblem {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = a + b;
+        out.println(c);
+    }
+}
