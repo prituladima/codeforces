a04a58cf10572af9d7942ccf27fff3d28cbbc569
Dmytro Prytula <dmytro_prytula@epam.com>
2019-11-27 12:36:34 +0200

    TIMUS - 66 solved

---
+++ src/main/java/com/prituladima/timus/Task2035.java
@@ -0,0 +1,282 @@
+package com.prituladima.timus;
+
+import java.io.*;
+import java.util.*;
+
+import static java.util.Arrays.stream;
+import static java.util.stream.IntStream.range;
+
+public class Task2035 {
+
+    private static final int BITS = 31;
+    private static final int MODULO = (int) 1e9 + 7;
+    private static final int INF = (int) 1e7 + 7;
+
+    private static final String yes = "YES", no = "NO";
+    private static final boolean ONLINE_JUDGE = System.getProperty("ONLINE_JUDGE") != null;
+    private static final boolean MULTI_TEST = false;
+
+    private void solve() {
+
+        int a = nextInt();
+        int b = nextInt();
+        int c = nextInt();
+
+        if (possible(a, b, c)) {
+            printf("%d %d\n", a, c - a);
+        } else if (possible(b, a, c)) {
+            printf("%d %d\n", c - b, b);
+        }
+        else if(a >= c && b >= c){
+            printf("%d %d\n", c, 0);
+        }else {
+            printf("%s\n", "Impossible");
+        }
+
+    }
+
+    private boolean possible(int a, int b, int c) {
+        return 0 <= c - a && c - a <= b;
+    }
+
+    private void solveAll() {
+        int t = MULTI_TEST ? nextInt() : 1;
+        while (t-- > 0) {
+            solve();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Task2035().run();
+    }
+
+    private BufferedReader reader;
+    private PrintWriter writer;
+    private StringTokenizer tokenizer;
+
+    private void run() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)))) {
+            this.writer = writer;
+            this.reader = reader;
+            solveAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Base types: Strings, int, long, double
+     */
+    private String nextToken() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+
+    private int nextInt() {
+        return Integer.parseInt(nextToken());
+    }
+
+    private long nextLong() {
+        return Long.parseLong(nextToken());
+    }
+
+    private double nextDouble() {
+        return Double.parseDouble(nextToken());
+    }
+
+
+    /**
+     * Primitives 1D arrays: char, int, long, double
+     */
+    private char[] nextCharArray() {
+        return nextToken().toCharArray();
+    }
+
+    private int[] nextIntArray(int size) {
+        return stream(new int[size]).map(c -> nextInt()).toArray();
+    }
+
+    private long[] nextLongArray(int size) {
+        return stream(new long[size]).map(c -> nextLong()).toArray();
+    }
+
+    private double[] nextDoubleArray(int size) {
+        return stream(new double[size]).map(c -> nextDouble()).toArray();
+    }
+
+    private String[] nextStringArray(int size) {
+        return range(0, size).mapToObj(i -> nextToken()).toArray(String[]::new);
+    }
+
+
+    /**
+     * Primitives 2D arrays: char, int, long, double
+     */
+    private char[][] nextCharMatrix(int n) {
+        return range(0, n).mapToObj(i -> nextToken().toCharArray()).toArray(char[][]::new);
+    }
+
+    private int[][] nextIntMatrix(final int n, final int m) {
+        return range(0, n).mapToObj(i -> nextIntArray(m)).toArray(int[][]::new);
+    }
+
+    private long[][] nextLongMatrix(final int n, final int m) {
+        return range(0, n).mapToObj(i -> nextLongArray(m)).toArray(long[][]::new);
+    }
+
+    private double[][] nextDoubleMatrix(final int n, final int m) {
+        return range(0, n).mapToObj(i -> nextDoubleArray(m)).toArray(double[][]::new);
+    }
+
+    /**
+     * Output
+     */
+    private void printf(String format, Object... args) {
+        writer.printf(format, args);
+    }
+
+    private void print(Object o) {
+        writer.print(o);
+    }
+
+    private void println() {
+        writer.println();
+    }
+
+    private void println(Object o) {
+        writer.println(o);
+    }
+
+    private void flush() {
+        writer.flush();
+    }
+
+    /**
+     * Utils
+     */
+
+    private boolean isValidIndex(int ind, int n) {
+        return 0 <= ind && ind < n;
+    }
+
+    private void debug(Object o) {
+        if (ONLINE_JUDGE) return;
+        println(o);
+    }
+
+    private void debugMatrix(int[][] matrix) {
+        if (ONLINE_JUDGE) return;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                print(matrix[i][j]);
+                print(' ');
+            }
+            println();
+        }
+    }
+
+    public static double maxn(double req, double... opt) {
+        double max = req;
+        for (double value : opt) max = Math.max(max, value);
+        return max;
+    }
+
+    public static double minn(double req, double... opt) {
+        double min = req;
+        for (double value : opt) min = Math.min(min, value);
+        return min;
+    }
+
+    public static double sumn(double... a) {
+        return stream(a).sum();
+    }
+
+    public static int maxn(int req, int... opt) {
+        int max = req;
+        for (int value : opt) max = Math.max(max, value);
+        return max;
+    }
+
+    public static int minn(int req, int... opt) {
+        int min = req;
+        for (int value : opt) min = Math.min(min, value);
+        return min;
+    }
+
+    public static int sumn(int... a) {
+        return stream(a).sum();
+    }
+
+    public static long maxn(long req, long... opt) {
+        long max = req;
+        for (long value : opt) max = Math.max(max, value);
+        return max;
+    }
+
+    public static long minn(long req, long... opt) {
+        long min = req;
+        for (long value : opt) min = Math.min(min, value);
+        return min;
+    }
+
+    public static long sumn(long... a) {
+        return stream(a).sum();
+    }
+
+    public static void safeSort(long[] array) {
+        shuffle(array);
+        Arrays.sort(array);
+    }
+
+    public static void shuffle(long[] array) {
+        Random random = new Random();
+        for (int i = 0, j; i < array.length; i++) {
+            j = i + random.nextInt(array.length - i);
+            long buf = array[j];
+            array[j] = array[i];
+            array[i] = buf;
+        }
+    }
+
+    public static void safeSort(int[] array) {
+        shuffle(array);
+        Arrays.sort(array);
+    }
+
+    public static void shuffle(int[] array) {
+        Random random = new Random();
+        for (int i = 0, j; i < array.length; i++) {
+            j = i + random.nextInt(array.length - i);
+            int buf = array[j];
+            array[j] = array[i];
+            array[i] = buf;
+        }
+    }
+
+    public static Map<Double, Integer> multiSet(double[] arr) {
+        Map<Double, Integer> multiSet = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) multiSet.put(arr[i], multiSet.getOrDefault(arr[i], 0) + 1);
+        return multiSet;
+    }
+
+    public static Map<Integer, Integer> multiSet(int[] arr) {
+        Map<Integer, Integer> multiSet = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) multiSet.put(arr[i], multiSet.getOrDefault(arr[i], 0) + 1);
+        return multiSet;
+    }
+
+    public static Map<Long, Integer> multiSet(long[] arr) {
+        Map<Long, Integer> multiSet = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) multiSet.put(arr[i], multiSet.getOrDefault(arr[i], 0) + 1);
+        return multiSet;
+    }
+
+}
